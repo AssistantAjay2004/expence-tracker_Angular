49e581d4f1babbc7f79c254a4f02918bfdfa21f6
Ajay Mishra <am3618571@gmail.com>
2025-12-10 13:34:06 +0530

    updated

--- src/app/budget-planner/income/income.component.ts
+++ src/app/budget-planner/income/income.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-income',
@@ -26,8 +27,10 @@ export class IncomeComponent {
     { source: 'Freelancing', amount: 1200, investments: 'Stocks' },
     { source: 'Rental Income', amount: 600, investments: 'Real Estate' }
   ];
+
  monthSelected:boolean = false;
-  constructor(public fb:FormBuilder){
+
+  constructor(public fb:FormBuilder,public router:Router){
     const currentDate = new Date();
     this.selectedMonth = currentDate.toLocaleString('default', { month: 'long' });
   }
@@ -43,6 +46,7 @@ export class IncomeComponent {
 
   onChange(event:any){
     this.selectedMonth = event.target.value
+    this.monthSelected = true;
     this.getFilteredIncome();
   }
 
@@ -85,7 +89,32 @@ export class IncomeComponent {
   }
 
   onSubmit(){
+     if (this.incomeForm.valid) {
+      const newIncome = this.incomeForm.value;
+      switch (this.selectedMonth) {
+        case 'January':
+          this.januaryIncome.push(newIncome);
+          break;
+        case 'Fabruary':
+          this.februaryIncome.push(newIncome);
+          break;
+        case 'March':
+          this.marchIncome.push(newIncome);
+          break;
+        default:
+          break;      
+      }
+      this.incomeForm.reset();
+      this.incomeForm.patchValue({ month:'', source:'', amount:'', investments: '' });
+     }
+  }
 
+  saveForm(){
+   console.log("Form saved!");
+  }
+
+  onBack(){
+   this.router.navigate(['/budget-planner/dashboard']);
   }
 
 }
